0faa75312d54ebe6bc55b7bf509103a96a9c4d41
jlyw1017 <jlyw1017@126.com>
2020-04-20 17:18:33 +0800

    hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <thread>
+
+void hello(){
+    std::cout << "Hello, My first thread!" << std::endl;
+}
+
+class helloFromObject{
+public:
+    void operator()() const{
+        std::cout << "Hello, My Second thread!" << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Coconut Cat!" << std::endl;
+    std::thread athread(hello);
+    std::thread bthread((helloFromObject()));
+    athread.join();
+    bthread.join();
     return 0;
 }
