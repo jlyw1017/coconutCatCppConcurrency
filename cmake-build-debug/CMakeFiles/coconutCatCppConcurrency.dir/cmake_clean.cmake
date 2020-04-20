1700d2e47e2e52c3b67b24884f27365756052ea1
jlyw1017 <jlyw1017@126.com>
2020-04-20 17:04:27 +0800

    project build

---
+++ cmake-build-debug/CMakeFiles/coconutCatCppConcurrency.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coconutCatCppConcurrency.dir/main.cpp.o"
+  "coconutCatCppConcurrency.pdb"
+  "coconutCatCppConcurrency"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coconutCatCppConcurrency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
